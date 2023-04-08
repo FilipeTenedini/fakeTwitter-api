46550308936264bdbd7f5588dd9a52b6add1946a
filipetenedini <tenedinifilipe@gmail.com>
2023-04-08 10:43:15 -0300

    (feat) implement receiving info by header

--- src/controllers/TweetController.js
+++ src/controllers/TweetController.js
@@ -3,23 +3,24 @@ import UsersRepository from '../repositories/UsersRepository.js';
 
 class TweetController {
   async create(req, res) {
-    const { username, tweet } = req.body;
+    const { tweet } = req.body;
+    const { user } = req.headers;
 
-    if (!username || !tweet) return res.status(400).send('Todos os campos são obrigatórios!');
+    if (!user || !tweet) return res.status(400).send('Todos os campos são obrigatórios!');
 
-    if (typeof (username) !== 'string' || typeof (tweet) !== 'string') return res.status(400).send('Todos os campos são obrigatórios!');
+    if (typeof (user) !== 'string' || typeof (tweet) !== 'string') return res.status(400).send('Todos os campos são obrigatórios!');
 
-    const userExist = await UsersRepository.findUserByName(username);
+    const userExist = await UsersRepository.findUserByName(user);
     if (!userExist) return res.status(401).send('UNAUTHORIZED');
 
-    await TweetsRepository.create({ username, tweet });
+    await TweetsRepository.create({ user, tweet });
 
     res.status(201).send('OK');
   }
 
   async show(req, res) {
-    const { username } = req.params;
-    const userTweets = await TweetsRepository.listByUser(username);
+    const { user } = req.params;
+    const userTweets = await TweetsRepository.listByUser(user);
     res.status(201).send(userTweets);
   }
 
@@ -32,8 +33,8 @@ class TweetController {
 
       if (page < 1) return res.status(400).send('Informe uma página válida!');
 
-      MIN = page * 10;
-      MAX = MIN + 10;
+      MAX = page * 10;
+      MIN = MAX - 10;
     }
 
     const tweetsList = await TweetsRepository.list(MIN, MAX);
